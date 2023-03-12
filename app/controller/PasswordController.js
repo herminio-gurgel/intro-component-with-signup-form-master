7ae864b165e1d71da04c6ad3dfaf56886359ad40
Herminio <herminio.gurgel@gmail.com>
2023-03-11 21:22:28 -0300

    comit parcial

--- app/controller/PasswordController.js
+++ app/controller/PasswordController.js
@@ -19,94 +19,102 @@ export class PasswordController {
 
     }
 
+    /*
+
+        validaSenha(pw) {
+            let issues = []
+            if (pw.length < 8) {
+                issues.push('be at least 8 letters long')
+            }
+            if (pw.length > 16) {
+
+                issues.push('be less than 16 letters long')
+            }
+            if (!pw.match(/[A-Z]/g)) {
+                issues.push('contain a capital letter')
+            }
+            if (!pw.match(/[a-z]/g)) {
+                issues.push('contain a lowercase letter')
+            }
+            if (!pw.match(/\d/g)) {
+                issues.push('contain a number')
+            }
+            if (!pw.match(/\W+/g)) {
+                issues.push('contain a special character')
+            }
+
+            return issues.length ? {
+                sentence: sentence(issues),
+                issues: issues
+            } : false;
+
+            function sentence(reasons) {
+                let start = 'Password must '
+                if (reasons.length === 1) {
+                    return start + reasons[0] + '.'
+                }
+                if (reasons.length === 2) {
+                    return start + reasons[0] + ' and ' + reasons[1] + '.'
+                }
+                if (reasons.length > 2) {
+                    let last = reasons[reasons.length - 1];
+                    return start + reasons.slice(0, -1).map(function (r) {
+                        return r;
+                    }).join(', ') + ', and ' + last + '.'
+                }
+            }
+        }
+
+     */
+
     validaSenha(pw) {
         let issues = []
         if (pw.length < 8) {
-            issues.push({
-                reason: 'minimumLength',
-                message: 'Password must be at least 8 letters long',
-                part: 'be at least 8 letters long'
-            });
+            issues.push('be at least 8 letters long')
         }
         if (pw.length > 16) {
 
-            issues.push({
-                reason: 'maximumLength',
-                message: 'Password must be less than 16 letters long',
-                part: 'be less than 16 letters long'
-            });
+            issues.push('be less than 16 letters long')
         }
         if (!pw.match(/[A-Z]/g)) {
-            issues.push({
-                reason: 'requireCapital',
-                message: 'Password must contain a capital letter',
-                part: 'contain a capital letter'
-            });
+            issues.push('contain a capital letter')
         }
         if (!pw.match(/[a-z]/g)) {
-            issues.push({
-                reason: 'requireLower',
-                message: 'Password must contain a lowercase letter',
-                part: 'contain a lowercase letter'
-            });
+            issues.push('contain a lowercase letter')
         }
         if (!pw.match(/\d/g)) {
-            issues.push({
-                reason: 'requireNumber',
-                message: 'Password must contain a number',
-                part: 'contain a number'
-            });
+            issues.push('contain a number')
         }
         if (!pw.match(/\W+/g)) {
-            issues.push({
-                reason: 'requireSpecial',
-                message: 'Password must contain a special character',
-                part: 'contain a special character'
-            });
+            issues.push('contain a special character')
         }
 
-        return issues.length ? {
-            sentence: sentence(issues),
-            issues: issues
-        } : false;
+        // console.log(issues.length)
 
-        function sentence(reasons) {
-            let start = 'Password must ';
-            if (reasons.length === 1) {
-                return start + reasons[0].part + '.';
-            }
-            if (reasons.length === 2) {
-                return start + reasons[0].part + ' and ' + reasons[1].part + '.';
-            }
-            if (reasons.length > 2) {
-                var last = reasons[reasons.length - 1].part;
-                return start + reasons.slice(0, -1).map(function (r) {
-                    return r.part;
-                }).join(', ') + ', and ' + last + '.';
-            }
+        // return issues.length ? {
+        //     sentence: this.sentence(issues),
+        //     // issues: issues
+        // } : false;
+        if (issues.length > 0) {
+            console.log(issues.length)
+            this.sentence(issues)
         }
-    };
-
-    validaTamanhoMinimo(pw){
-    }
-
-    validaTamanhoMaximo(pw){
 
     }
 
-    validaMaiusculo(pw){
-
-    }
-
-    validaMinuculo(pw){
-
-    }
-
-    validaNumero(pw){
-
-    }
-
-    validaEspecial(pw){
-
+    sentence(reasons) {
+        let start = 'Password must '
+        if (reasons.length === 1) {
+            return start + reasons[0] + '.'
+        }
+        if (reasons.length === 2) {
+            return start + reasons[0] + ' and ' + reasons[1] + '.'
+        }
+        if (reasons.length > 2) {
+            let last = reasons[reasons.length - 1];
+            return start + reasons.slice(0, -1).map(function (r) {
+                return r;
+            }).join(', ') + ', and ' + last + '.'
+        }
     }
 }
